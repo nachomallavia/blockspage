446a0cc1cad13fdf3aa4afa1839508cce2fc8167
nachomallavia <nachomallavia@hotmail.com>
2025-07-22 17:52:39 -0300

    a verr

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,9 +3,8 @@ import { defineMiddleware } from 'astro:middleware';
 export const onRequest = defineMiddleware((context, next) => {
     // 1. Get the hostname from the request URL.
     const hostname = context.url.hostname;
-    let nakedHostname = 'el hostname contiene www';
     if (hostname.includes('www.')) {
-        context.locals.hostname = nakedHostname;
+        context.locals.hostname = hostname.replace('www.', '');
     } else {
         context.locals.hostname = hostname;
     }
