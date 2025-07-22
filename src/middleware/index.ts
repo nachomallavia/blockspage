878b7834de2f771106103c4b7200157fc8ddc4a9
nachomallavia <nachomallavia@hotmail.com>
2025-07-22 17:16:48 -0300

    Nuev versión

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,18 +3,12 @@ import { defineMiddleware } from 'astro:middleware';
 export const onRequest = defineMiddleware((context, next) => {
     // 1. Get the hostname from the request URL.
     const hostname = context.url.hostname;
-    if (hostname.startsWith('www.')) {
-        const nakedHostname = hostname.replace('www.', '');
-        return Response.redirect(
-            new URL(context.url.pathname, `https://${nakedHostname}`),
-            301
-        );
+    let nakedHostname = 'el hostname contiene www';
+    if (hostname.includes('www.')) {
+        context.locals.hostname = nakedHostname;
+    } else {
+        context.locals.hostname = hostname;
     }
 
-    // 2. Attach the hostname to `context.locals`. This is a special object
-    //    that securely passes data from middleware to your Astro pages.
-    context.locals.hostname = hostname;
-
-    // 3. Continue to the next step (either another middleware or rendering the page).
     return next();
 });
